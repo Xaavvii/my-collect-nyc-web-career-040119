afb27b2a95456aaa3675f89f3e87a5923bdaeb6c
authentic-actor-8801 <xaviernational@aol.com>
2019-03-01 23:48:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array) # put argument(s) here
+  # code here
+  i = 0
+  new_array = []
+
+  while i < array.size
+    block_given?
+    new_array << yield(array[i])
+    i += 1
+  end
+new_array
+end
 
 
